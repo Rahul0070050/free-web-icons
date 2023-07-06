51388acbc471e4106ae70fff5add3d3b9b559f90
rahul <rahulor2000@gmail.com>
2023-07-06 11:33:51 +0530

    almost finished

--- media/main.js
+++ media/main.js
@@ -1,9 +1,2 @@
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
-
-(function () {
-    const vscode = acquireVsCodeApi();
-    document.getElementById('h1').addEventListener("click", (e) => {
-        console.log('hi');
-    });
-}());
