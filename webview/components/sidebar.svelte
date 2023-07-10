fb5748b4c261e19d2120e9839f1cda0861dd6448
rahul <rahulor2000@gmail.com>
2023-07-10 22:44:22 +0530

    icon custom edit finished
    added pop-up window for editing the icon

--- webview/components/sidebar.svelte
+++ webview/components/sidebar.svelte
@@ -1,11 +1,19 @@
 <script async>
     import FAIcons from "../media/data/fa-icons.json";
     import iconVersions from "../media/data/versions.json";
+    import {fly} from "svelte/transition";
 
     // global variables
     let selectedIconVersion = iconVersions.versions[0];
     let iconInfo = FAIcons[0]
-    window.addEventListener("DOMContentLoaded", () => {
+    let search = ""
+    let size = ""
+    let rotation = ""
+    let animation = ""
+    let color = "#cccccc"
+    let selectedIcon = ""
+    let selectedIconName = ""
+    window.addEventListener("DOMContentLoaded", (e) => {
 
         // selecting DOM elms
         const messageText = document.getElementById("message-text");
@@ -13,11 +21,12 @@
         const copyBtn = document.querySelector(".copy-cdn-link-btn");
         const iconListContainer = document.getElementById("icon-list");
         const searchInput = document.getElementById("search");
-        let search = ""
 
-        copyBtn.addEventListener("click", copyText);
-
-        async function copyText (e) {
+        window.copyText = function copyText(e) {
+            window.navigator.clipboard.writeText(`<i class="${selectedIcon} ${size && size} ${rotation && rotation} ${animation && animation}" style="color: ${color};" />`)
+        }
+        
+        window.openIconInfo = async function openIconInfo (e) {
             console.log(e.target.getAttribute("data-attar"));
             let message = ""
             let content = ""
@@ -43,10 +52,14 @@
             }, 1000);
         }
 
-        showIcons(iconVersions.versions[0]);
-        showVersions();
+        window.openIconInfo = function openIconInfo(e) {
+            console.log(e.target);
+            selectedIcon = e.target.getAttribute('data-attar')
+            selectedIconName = e.target.getAttribute('title').replace('-',' ').toUpperCase()
+        }
 
-        console.log(searchInput);
+        showVersions();
+        showIcons(iconVersions.versions[0]);
 
         searchInput.addEventListener('input',(e) => {
             search = e.target.value;
@@ -64,32 +77,71 @@
         function showIcons(version) {
             iconInfo = FAIcons.find((icons) => icons.version == version);
             
-            iconListContainer.innerHTML = ``
-                
-            iconInfo?.icons?.map(icon => {
-                console.log(search != "");
-                console.log(search);
+            // iconListContainer.innerHTML = ``
+
+            iconInfo?.icons?.map((icon,i) => {
                 if(search != "") {
                     if(!icon.name.startsWith(search)) {
                         return null
                     }
                 }
+
                 let iTag = document.createElement('i');
-                iTag.setAttribute("class",`${icon.showIcon} svelte-1fcwvos`);
+                iTag.setAttribute("class",`${icon.showIcon} svelte-1wf2oa9`);
                 iTag.setAttribute("data-attar",`${icon.showIcon}`);
                 iTag.setAttribute("title", icon.name);
-                iTag.addEventListener('click',copyText)
+                iTag.addEventListener('click',openIconInfo)
                 iconListContainer.appendChild(iTag)
             })
             // `<i class="fa-solid fa-house" title="home" />`
         }
-
+        
+        
         
         iconVersionSelector.addEventListener("change", (e) => {
             selectedIconVersion = e.target.value;
             showIcons(selectedIconVersion);
         });
     });
+
+    function openIconInfo(e) {
+        window.openIconInfo(e)
+    }
+
+    function copyText(e) {
+        window.copyText(e)
+    }
+
+    function selectedFilter(e) {
+        console.log(e.target.name);
+        console.log(e.target.value);
+
+        if(e.target.name == "color") {
+            color = e.target.value
+        }
+        if(e.target.name == "size") {
+            size = e.target.value
+        }
+        if(e.target.name == "rotate") {
+            rotation = e.target.value
+        }
+        if(e.target.name == "animate") {
+            animation = e.target.value
+        }
+    }
+
+
+    function  closeIconInfoWindow() {
+        selectedIcon = ""
+        size = ""
+        rotation = ""
+        animation = ""
+    }
+
+    function copyCDN() {
+        window.navigator.clipboard.writeText(iconInfo.cdn)
+    }
+    
 </script>
 
 <body>
@@ -103,23 +155,86 @@
                 <!-- <label for="select-version">v: &nbsp;</label> -->
                 <select name="select-version" id="select-version" />
             </div>
-            <span data-attar="cdn-copy-btn" class="copy-cdn-link-btn">
+            <span data-attar="cdn-copy-btn" class="copy-cdn-link-btn" on:click={copyCDN}>
                 <i data-attar="cdn-copy-btn" class="fa-solid fa-link" />
             </span>
         </div>
     </nav>
     <section class="list-icons">
-            <div class="icons-container">
+        <div class="icons-container">
             <div class="icon-list" id="icon-list">
+                <i class="fa-solid fa-house" title="home" />
             </div>
+            {#if selectedIcon != "" }
+                <div class="icon-info-container" transition:fly={{y: -20}}>
+                    <div class="headder">
+                        <h3>{selectedIconName}</h3> <i on:click={closeIconInfoWindow} class="fa-solid fa-xmark"></i>
+                    </div>
+                    <div class="icon-preview">
+                        <i class="{selectedIcon} {size} {animation} {rotation}" style="color: {color};" title="home" on:click={copyText} />
+                    </div>
+                    <div class="actions">
+                        <div class="form-control">
+                            <input type="color" value="#cccccc" name="color" id="color" on:change={selectedFilter}>
+                        </div>
+                        <div class="form-control">
+                            <label for="size">size</label>
+                            <select name="size" id="size" on:click={selectedFilter}>
+                                <option value="none">none</option>
+                                <option value="fa-sm">sm</option>
+                                <option value="fa-gl">gl</option>
+                                <option value="fa-xl">xl</option>
+                                <option value="fa-2xl">2xl</option>
+                            </select>
+                        </div>
+                        <div class="form-control">
+                            <label for="rotate">rotate</label>
+                            <select name="rotate" id="rotate" on:click={selectedFilter}>
+                                <option value="">none</option>
+                                <option value="fa-rotate-90">rotate 90</option>
+                                <option value="fa-rotate-180">rotate 180</option>
+                                <option value="fa-rotate-270">rotate 90</option>
+                                <option value="fa-flip-horizontal">flip horizontal</option>
+                                <option value="fa-flip-both">flip vertical</option>
+                            </select>
+                        </div>
+                        <div class="form-control">
+                            <label for="animation">animate</label>
+                            <select name="animate" id="animate" on:click={selectedFilter}>
+                                <option value="">none</option>
+                                <option value="fa-beat">beat</option>
+                                <option value="fa-beat-fade">beat & fade</option>
+                                <option value="fa-bounce">bounce</option>
+                                <option value="fa-fade">fade</option>
+                                <option value="fa-flip">flip</option>
+                                <option value="fa-spin">spin</option>
+                                <option value="fa-shake">shake</option>
+                                <option value="fa-spin-pulse">spin pulse</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="copy" on:click={copyText}>
+                        <span>copy</span>
+                    </div>
+                </div>
+            {/if}
         </div>
     </section>
     <div id="message-text"></div>
 </body>
 
 <style>
-    body nav .select-version-and-search {
+    body {
+        position: relative;
+    }
+    body nav {
         width: 100% !important;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    body nav .select-version-and-search {
+        max-width: 17.5rem;
         display: flex;
         align-items: center;
         justify-content: space-around;
@@ -174,15 +289,107 @@
         margin-top: 0.5rem !important;
     }
     body section .icons-container .icon-list {
-        width: 90%;
+        max-width: 17.5rem;
     }
     body section .icons-container .icon-list i {
         cursor: pointer;
         font-size: medium;
         padding: 0.7rem !important;
         margin: 0.1rem !important;
-        width: 2.5rem !important;
-        box-shadow: inset rgba(245, 222, 179, 0.346) 0 0 3px 2px !important;
+        width: 2.5rem;
+        box-shadow: inset rgba(245, 222, 179, 0.346) 0 0 3px 2px;
+    }
+    body section .icon-info-container {
+        max-width: 16rem;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        height: fit-content;
+        width:  95%;
+        margin: 0.3rem 0 !important;
+        padding: 1rem 0 !important;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        box-shadow: inset  0 0 2px 2px rgba(173, 216, 230, 0.269);
+        background-color: black;
+    }
+    body section .icon-info-container .headder {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+        padding: 0.1rem 1rem 1rem 1rem !important;
+    }
+    body section .icon-info-container .headder i {
+        padding: 0.4rem 0.5rem !important;
+        border: 1px solid rgba(255, 255, 255, 0.542);
+        color: rgba(255, 255, 255, 0.512);
+        border-radius: 4px;
+        transition: all 0.3s ease;
+    }
+    body section .icon-info-container .headder i:hover {
+        border-color: white;
+        color: white;
+    }
+    body section .icon-info-container .icon-preview{
+        border: 1px solid rgba(245, 222, 179, 0.317);
+        width: 90%;
+        height: 10rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    body section .icon-info-container .icon-preview i {
+        cursor: pointer;
+        box-shadow: none;
+    }
+    body section .icon-info-container .actions {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 90% !important;
+        padding: 1rem 0 !important;
+    }
+    body section .icon-info-container .actions .form-control {
+        display: flex;
+        flex-direction: column;
+        width: 100% !important;
+        padding: 0.2rem 0 !important;
+    }
+    /* body section .icon-info-container .actions .form-control select:se */
+    body section .icon-info-container .actions .form-control label {
+        font-size: larger;
+        text-transform: capitalize;
+        padding: 0.3rem 0.1rem !important;
+    }
+
+    /* remove down arrow from dropdown */
+    body section .icon-info-container .actions .form-control select {}
+    /* remove down arrow from dropdown */
+
+    body section .icon-info-container .actions .form-control select {
+        padding: 0.2rem !important;
+        border-radius: 4px;
+        font-size: larger;
+        color: black;
+    }
+    body section .icon-info-container .actions .form-control input {
+        font-size: x-large !important;
+    }
+    body section .icon-info-container .actions .form-control select,
+    body section .icon-info-container .actions .form-control input {
+        width: 100% !important;
+    }
+    .copy {
+        padding: 0.4rem 0.5rem !important;
+        width: 90%;
+        color: black;
+        text-align: center;
+        font-size: larger;
+        background-color: lemonchiffon;
     }
     body #message-text {
         position: absolute;
